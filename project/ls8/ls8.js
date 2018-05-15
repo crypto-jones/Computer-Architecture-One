020ca28f05c2c4b9532818b8d374139c52208a85
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-05-15 14:36:31 -0700

    Steps 1-6 complete

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+// const MULT = require('./mult.ls8');
 
 /**
  * Load an LS8 program into memory
@@ -11,12 +12,26 @@ function loadMemory() {
 
   const program = [
     // print8.ls8
-    '10011001', // LDI R0,8  Store 8 into R0
+    // '10011001', // LDI R0,8  Store 8 into R0
+    // '00000000',
+    // '00001000',
+    // '01000011', // PRN R0    Print the value in R0
+    // '00000000',
+    // '00000001' // HLT       Halt and quit
+
+    // mult.ls8
+    '10011001', // LDI R0,8  Load R0 with value 8
     '00000000',
     '00001000',
-    '01000011', // PRN R0    Print the value in R0
+    '10011001', // LDI R1,9  Load R1 with value 9
+    '00000001',
+    '00001001',
+    '10101010', // MUL R0,R1 Multiply R0*R1, storing result in R0
     '00000000',
-    '00000001' // HLT       Halt and quit
+    '00000001',
+    '01000011', // PRN R0    Print value in R0
+    '00000000',
+    '00000001' // HLT       Halt
   ];
 
   // Load the program into the CPU's memory a byte at a time
